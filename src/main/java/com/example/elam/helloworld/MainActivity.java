94b487a90ddce59d225cb9264c39bdc1e1087122
Navinbala5 <bnavin28@gmail.com>
2017-07-05 11:32:28 +0530

    Add files via upload

---
+++ src/main/java/com/example/elam/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.elam.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void startBlinkingAnimation(View view) {
+        Button button = (Button) findViewById(R.id.hello);
+        Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.hello);
+        button.startAnimation(startAnimation);
+    }
+}
